6ab3f2d02ce7d27b9daf9e91f1353048cfdbfd39
nifle3 <kuper116rus.2005@gmail.com>
2024-10-21 07:51:26 +0300

    add: check uptime

--- Makefile
+++ Makefile
@@ -2,12 +2,15 @@
 
 run:
 	go build -o ./bin/main-test ./cmd/core/main.go
-	./bin/main.go
+	./bin/main-test
 
 test:
 	go test -v ./...
 
+BUILD_TARGETS := linux-amd64 freebsd-amd64 windows-amd64 darwin-arm64
+
 build:
-	GOOS=linux GOARCH=amd64 go build -o ./bin/main-linux-amd64 ./cmd/core/main.go
-	GOOS=freebsd GOARCH=amd64 go build -o ./bin/main-freebsd-amd64 ./cmd/core/main.go
-	GOOS=windows GOARCH=amd64 go build -o ./bin/main-windows-amd64.exe ./cmd/core/main.go
+	$(foreach target,$(BUILD_TARGETS),\
+		$(eval OS = $(word 1,$(subst -, ,$(target))))\
+		$(eval ARCH = $(word 2,$(subst -, ,$(target))))\
+		GOOS=$(OS) GOARCH=$(ARCH) go build -o ./bin/main-$(target)$(if $(filter windows,$(OS)),.exe,) ./cmd/core/main.go;)
--- cmd/core/main.go
+++ cmd/core/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"time"
 )
 
-func getTotalMemory() {
+func getTotalCPU() {
 	result, err := os.ReadFile("/proc/stat")
 	if err != nil {
 		fmt.Printf("err: %s", err.Error())
@@ -15,6 +17,54 @@ func getTotalMemory() {
 	fmt.Println(string(result))
 }
 
+func getTotalMemory() {
+	_, err := os.Stat("/")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
+
+func getTotalUser() {
+
+}
+
+func getProcessCount() {
+
+}
+
+type UptimeInfo struct {
+	idleTime time.Duration
+	allTime  time.Duration
+}
+
+func showUptime() (UptimeInfo, error) {
+	fileData, err := os.ReadFile("/proc/uptime")
+	if err != nil {
+		return UptimeInfo{}, err
+	}
+
+	var allTime float64
+	var idleTime float64
+	if _, err := fmt.Sscanf(string(fileData), "%f %f", &allTime, &idleTime); err != nil {
+		return UptimeInfo{}, err
+	}
+
+	allTimeDuration := ParseTimeDuration(allTime)
+	fmt.Println(allTimeDuration)
+	doo := ParseTimeDuration(idleTime)
+	fmt.Println(doo)
+
+	return UptimeInfo{}, nil
+}
+
+func ParseTimeDuration(uptime float64) float64 {
+	hours := uptime / 180.0
+	hoursTwoSymbol := math.Round(hours*100) / 100
+	return hoursTwoSymbol
+}
+
 func main() {
 	getTotalMemory()
+	showUptime()
 }
